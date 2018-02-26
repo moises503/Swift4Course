fc1aba96c46148ed07e3f1344e5f92a8e5fda063
Moises Garduño <moisesgarduno.backenddeveloper@gmail.com>
2018-02-26 00:34:12 -0600

    Add how to define a function in swift

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,11 @@
+func printInScreen(greeting : String){
+    print(greeting)
+}
+
+printInScreen(greeting: "hello swift 4")
+
+func printGreeting() -> String{
+    return "Hello my best friend"
+}
+
+printGreeting()
